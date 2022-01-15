d9b667ad8d9fb26cf8d7f1f9608e837b227a7108
arif ahmod <arifahmod9c@gmial.com>
2022-01-15 21:16:33 +0600

    practised onclick event

---
+++ custom.js
@@ -0,0 +1,12 @@
+"strict";
+
+let images = document.querySelector("img");
+
+function bike() {
+    images.src = "images/Bullet-350-Bike-cb-background-819x1024.jpg";
+
+}
+function car() {
+    images.src = "images/CB-BACKGROUND-13-716x1024.jpg";
+   
+}
