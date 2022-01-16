9736c86acb5ad57683b6f9c43be18a1615809f6c
arif ahmod <arifahmod9c@gmial.com>
2022-01-16 12:29:59 +0600

    changing background by clicking button

--- custom.js
+++ custom.js
@@ -4,29 +4,22 @@ let photos = ["images/bike.jpg", "images/car.jpg", "images/laptop.jpeg"];
 let imgTag = document.querySelector("img");
 let count = 0;
 
+let background1 = document.querySelector("body");
 
 function next() {
-   count++;
-   if(count >= photos.length) {
-    count = 0;
-    imgTag.src = photos[count];
 
-   }else{
-       imgTag.src = photos[count];
+   background1.style.background = "black";
+   background1.style.color = "white";
+   alert("you have clicked on black button");
+
    }
 
-}
-
-
-
 function prev() {
-    count--;
-    if(count < 0) {
-     count = photos.length - 1;
-     imgTag.src = photos[count];
- 
-    }else{
-        imgTag.src = photos[count];
-    }
+
+    background1.style.background = "red";
+   background1.style.color = "white";
+
+   alert("You have clicked on white button");
+  
     
 }
