70b9659979033e223c36091ef21f8dd0b0f7e348
arif ahmod <arifahmod9c@gmial.com>
2022-01-16 12:20:02 +0600

    self practise

--- custom.js
+++ custom.js
@@ -1,31 +1,31 @@
 "strict";
 
-//variable declared
 let photos = ["images/bike.jpg", "images/car.jpg", "images/laptop.jpeg"];
 let imgTag = document.querySelector("img");
 let count = 0;
 
+
 function next() {
-    count++;
-if (count >= photos.length) {
+   count++;
+   if(count >= photos.length) {
     count = 0;
-    imgTag.src=photos[count];
-
-} else{
-
     imgTag.src = photos[count];
-}
+
+   }else{
+       imgTag.src = photos[count];
+   }
 
 }
+
+
 
 function prev() {
-    count --;
-    if (count < 0) {
-       count=photos.length - 1;
-        imgTag.src=photos[count];
-    
-    } else{
-    
+    count--;
+    if(count < 0) {
+     count = photos.length - 1;
+     imgTag.src = photos[count];
+ 
+    }else{
         imgTag.src = photos[count];
     }
     
