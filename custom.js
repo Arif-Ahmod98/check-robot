bdd1b0d8331d4ffde44007990115e8816aeee4c9
arif ahmod <arifahmod9c@gmial.com>
2022-01-16 16:05:59 +0600

    slider images

--- custom.js
+++ custom.js
@@ -1,34 +1,32 @@
 "strict";
 
-let images = ["images/bike.jpg", "images/car.jpg", "images/laptop.jpeg", "images/vactor.png", "images/jeep.jpg", "images/flower.jfif", "images/sunset.jfif", "images/cow.jfif"];
+let photos = ["images/bike.jpg", "images/car.jpg", "images/laptop.jpeg", "images/flower.jfif", "images/cow.jfif"];
 
 let imgSource = document.querySelector("img");
-
 let count = 0;
+
+
 
 function next() {
     count++;
-    if (count >= images.length) {
-        count = 0;
-        imgSource.src = images[count];
+if  ( count >= photos.length ) {
+    count = 0;
+    imgSource.src = photos[count];
 
-    } else {
-        imgSource.src = images[count];
-    }
+} else {
+    imgSource.src = photos[count];
 
-
-
+}
 }
 
 function prev() {
-    count--;
-    if (count < 0) {
-        count = images.length - 1;
-        imgSource.src = images[count];
-
-    } else {
-        imgSource.src = images[count];
-    }
-
+    count --;
+    if  ( count < 0 ) {
+        count = photos.length - 1;
+        imgSource.src = photos[count];
     
+    } else {
+        imgSource.src = photos[count];
+    
+}
 }
